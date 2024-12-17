6d14a4fa3cde6ef4030485542e8c3b4fccb873a0
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2024-12-17 15:42:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_with_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the following information:");
+            Console.WriteLine("T.C. Your ID Number:");
+            //TC kimlik numarası için string kullandım çünkü  Kimlik numarası ile herhangi bir matematiksel işlem yapılmaz.
+            string tcNumber = Console.ReadLine();
+            Console.WriteLine("Your Name:");
+            string firstName= Console.ReadLine();
+            Console.WriteLine("Your LastName:");
+            string lastName =Console.ReadLine();
+            //Telefon numarası string kullandım çünkü Phone numarası ile herhangi bir matematiksel işlem yapılmaz.
+            Console.WriteLine("Your Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            //Console.readline sadece metin girişidir.bu yüzden tryParse kullanıp intager'a çevirdim.
+            Console.WriteLine("The price of the first product you purchased");
+            string firstProduct = Console.ReadLine();
+            bool tryParse1 = Int32.TryParse(firstProduct, out int product1);
+            
+            Console.WriteLine("The price of the second product you purchased");
+            string secondProduct = Console.ReadLine();
+            bool tryParse2 = Int32.TryParse(firstProduct, out int product2);
+
+            int percentage = 10;       int sum = product1 + product2;        int result = (sum * percentage) / 100;
+
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine($"{tcNumber}, TC numbered, {firstName}, A record has been created for the person named.");
+            Console.WriteLine($"{phoneNumber}, A notification message has been sent to the phone number.");
+            Console.WriteLine($"{sum}, 10% amount of trail points earned for total spending is->{result}");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Application with Variables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Application with Variables")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("08a133c0-74bd-47e4-843d-779446274748")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
